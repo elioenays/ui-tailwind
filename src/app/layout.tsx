0811e2155d11b5e14ccfd0543fe579721bcb76eb
Elioenay Silva <elionay407@gmail.com>
2023-04-20 11:57:22 -0300

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,17 +18,18 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={inter.className}>
-      <body className="bg-zinc-900">
+    <html
+      lang='en'
+      className={inter.className}
+    >
+      <body className='bg-zinc-900'>
         <Sidebar />
 
-        <div className="ml-80 relative h-screen">
+        <div className='ml-80 relative h-screen'>
           <Header />
           <HeroPattern />
 
-          <div className="py-24 max-w-4xl px-8 mx-auto">
-            {children}
-          </div>
+          <div className='py-24 max-w-4xl px-8 mx-auto'>{children}</div>
         </div>
       </body>
     </html>
